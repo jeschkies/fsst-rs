b30b0bd5025bb4faa9f0afbe035ab1f158551a95
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-02-19 14:31:57 +0100

    Format code

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,22 @@
-
 mod fsst;
 #[cfg(test)]
 mod tests {
-    use std::ffi::CString;
     use crate::fsst;
+    use std::ffi::CString;
 
     #[test]
     fn it_works() {
-
         unsafe {
             let mut inputs = vec![CString::new("Hello").unwrap()];
 
             let mut lengths = vec![5];
 
-            let _ = fsst::fsst_create(1, lengths.as_mut_ptr(), inputs.as_mut_ptr() as *mut *mut u8, 1);
+            let _ = fsst::fsst_create(
+                1,
+                lengths.as_mut_ptr(),
+                inputs.as_mut_ptr() as *mut *mut u8,
+                1,
+            );
         }
     }
 }
