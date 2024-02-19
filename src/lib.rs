28d24f6f13d207d951ef1561ad50f67d6744ee02
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-02-19 11:58:35 +0100

    Generate functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 mod tests {
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let encoder = fsst_create();
+        //assert_eq!(result, 4);
     }
 }
