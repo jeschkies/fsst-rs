3d4db597be1d44488da8b60ce51d1c86b15a8fa9
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-02-20 13:02:20 +0100

    compress

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,26 @@ mod tests {
 
             let mut lengths = vec![5];
 
-            let _ = fsst::fsst_create(
+            let encoder = fsst::fsst_create(
                 1,
                 lengths.as_mut_ptr(),
                 inputs.as_mut_ptr() as *mut *mut u8,
                 1,
+            );
+
+            let outsize = 128;
+            let mut out = Vec::with_capacity(outsize);
+            let mut out_lengths = Vec::with_capacity(1);
+            let mut out_strs = vec![CString::new("Hello").unwrap()];
+            let _ = fsst::fsst_compress(
+                encoder,
+                1,
+                lengths.as_mut_ptr(),
+                inputs.as_mut_ptr() as *mut *mut u8,
+                outsize,
+                out.as_mut_ptr() as *mut u8,
+                out_lengths.as_mut_ptr() as *mut usize,
+                out_strs.as_mut_ptr() as *mut *mut u8,
             );
         }
     }
